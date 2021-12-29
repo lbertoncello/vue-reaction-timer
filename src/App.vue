dce3dc32b1d7cf0ce8d383fdbf6f662d20a94984
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-29 19:03:23 -0300

    feat(app): add data and  methods

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start">Play</button>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      // An amount time between 1s and 4s
+      this.delay = 1000 + (Math.random() * 3000)
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  }
 }
 </script>
 
