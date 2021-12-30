c6a9261de7dec7c8eb53029f41fbfa2794f87441
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-30 15:13:10 -0300

    feat(Results): add ranking

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
   <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: [ 'score' ]
+  props: [ 'score' ],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if(this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if(this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail Pace...'
+    }
+  }
 }
 </script>
 
